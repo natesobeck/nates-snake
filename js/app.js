e434e6d37df37636e7a72631dfef9cbe951f6e51
Nate Sobeck <nsobeck910@gmail.com>
2023-09-05 09:54:27 -0400

    get snake working with moveOnce edit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 //*CONSTANTS
 class Cell {
-  constructor(x, y, direction = 'left') {
+  constructor(x, y) {
     this.x = x
     this.y = y
-    this.direction = direction
   }
 }
 
@@ -30,12 +29,10 @@ class Apple {
 }
 
 class Snake {
-  constructor(x, y) {
+  constructor(x, y, direction = 'left') {
     this.length = 3
     this.coordinates = [new Cell (x, y), new Cell(x, y + 1), new Cell(x, y + 2)]
-    this.body = this.coordinates.slice(1)
-    this.head = this.coordinates[0]
-    this.timer = 0
+    this.direction = direction
   }
   display() {
     boardCellEls.forEach(displayedCell => {
@@ -48,45 +45,29 @@ class Snake {
     })
   }
   moveOnce() {
-    this.coordinates.forEach(coordinate => {
-      if(coordinate.direction === 'left') {
-        coordinate.y -= 1
-      } else if(coordinate.direction === 'up') {
-        coordinate.x -= 1
-      } else if(coordinate.direction === 'right') {
-        coordinate.y += 1
-      } else {
-        coordinate.x += 1
-      }
-    })
+    let cloneHead = Object.assign({}, this.coordinates[0])
+    if (this.direction === 'left') {
+      cloneHead.y -= 1
+    } else if(this.direction === 'up') {
+      cloneHead.x -= 1
+    } else if(this.direction === 'right') {
+      cloneHead.y += 1
+    } else {
+      cloneHead.x += 1
+    }
+    this.coordinates.unshift(cloneHead)
+    this.coordinates.pop()
     clearBoard()
     this.display()
   }
   moveContinuously() {
-    const moveTimer = setInterval(this.moveOnce.bind(this), 250)
+    const moveTimer = setInterval(this.moveOnce.bind(this), 150)
   }
-  changeHeadDirection(direction) {
-    this.coordinates[0].direction = direction
-  }
-}
-
-function getCoordinates(displayedCell) {
-  const cellId = displayedCell.getAttribute('id')
-  const x = parseInt(cellId[4] + cellId[5])
-  const y = parseInt(cellId[7] + cellId[8])
-  const newCell = new Cell(x, y)
-  return newCell
-}
-
-function clearBoard() {
-  boardCellEls.forEach(cellEl => {
-    if (!cellEl.classList.contains('apple'))
-    cellEl.className = 'cell'
-})
 }
 
 //*STATE VARIABLES
-let board = [];
+let board = []
+let tempArray = []
 let score, snake, apple
 
 //*CACHED ELEMENT REFERENCES
@@ -140,13 +121,13 @@ function generateBoardCells() {
 
 function handleArrowKeydown(e) {
   if (e.key === 'ArrowUp') {
-    snake.coordinates[0].direction = 'up'
+    snake.direction = 'up'
   } else if (e.key === 'ArrowDown') {
-    snake.coordinates[0].direction = 'down'
+    snake.direction = 'down'
   } else if (e.key === 'ArrowRight') {
-    snake.coordinates[0].direction = 'right'
+    snake.direction = 'right'
   } else if (e.key === 'ArrowLeft') {
-    snake.coordinates[0].direction = 'left'
+    snake.direction = 'left'
   }
 }
 
@@ -172,6 +153,21 @@ function randomIndex() {
   return index + 1
 }
 
+function clearBoard() {
+  boardCellEls.forEach(cellEl => {
+    if (!cellEl.classList.contains('apple'))
+    cellEl.className = 'cell'
+})
+}
+
+function getCoordinates(displayedCell) {
+  const cellId = displayedCell.getAttribute('id')
+  const x = parseInt(cellId[4] + cellId[5])
+  const y = parseInt(cellId[7] + cellId[8])
+  const newCell = new Cell(x, y)
+  return newCell
+}
+
 function incrementScore(score) {
 }
 
@@ -194,6 +190,7 @@ function displayResult() {
 
 //*SOURCES
 
-//*For running keydown eveent listeners
+//*For running keydown event listeners
 // https://www.javascripttutorial.net/javascript-dom/javascript-keyboard-events/
 
+// https://stackoverflow.com/questions/56496376/get-previous-item-seen-in-js-for-of-loop
